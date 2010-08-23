211995e5a79c630127cbe5529b7a2fcb595f5874
cko <ck@.(none)>
2010-08-23 22:05:05 +0200

    refactored views

--- techism2/events/views.py
+++ techism2/events/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render_to_response
 from techism2.events.models import Event
 from django.http import HttpResponse
+from datetime import datetime
 
 def index(request):
-    latest_event_list = Event.objects.all().order_by('-dateBegin')[:10]
+    latest_event_list = Event.objects.filter(dateBegin__gte=datetime.today()).order_by('dateBegin')[:10]
     return render_to_response('events/index.html', {'latest_event_list': latest_event_list})
 
 def detail(request, event_id):
-    event = Event.objects.get(id=event_id);
+    event = Event.objects.get(id=event_id)
     return render_to_response('events/detail.html', {'event':event})
 
 def results(request, event_id):
