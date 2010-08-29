40cf987dd743d0d774df8aec5385d52c4de519f4
cko <ck@.(none)>
2010-08-29 23:45:26 +0200

    extended event creation form

--- techism2/events/forms.py
+++ techism2/events/forms.py
@@ -1,8 +1,18 @@
 from django import forms
 from django.forms import ModelForm
-from techism2.events.models import Event
+from techism2.events.models import Address
 
-class EventForm(ModelForm):
-    class Meta:
-        model = Event
-        exclude = ['location']
+class EventForm(forms.Form):
+    title = forms.CharField(max_length=200, label= 'Titel')
+    dateBegin = forms.DateTimeField(label= 'Beginn')
+    dateEnd = forms.DateTimeField(label= 'Ende',required=False)
+    url = forms.URLField()
+    description = forms.CharField(label= 'Beschreibung', widget=forms.Textarea )
+    #location = forms.ModelChoiceField (Address.objects.all(), required=False)
+    
+    #name = forms.CharField(max_length=200, required=False)
+    #street = forms.CharField(max_length=200, required=False)
+    #city = forms.CharField(max_length=200,required=False)
+    #latitude = forms.FloatField(required=False)
+    #longitude = forms.FloatField(required=False)
+  
