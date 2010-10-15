23722023aa849ce3324c6f248f02d01776f38fc5
seelmann <mail@stefan-seelmann.de>
2010-10-15 21:24:04 +0200

    Set UTF-8 enoding and non-caching headers for iCal feed

--- techism2/ical/views.py
+++ techism2/ical/views.py
@@ -62,6 +62,9 @@ def ical(request):
         cal.add_component(event)
     
     response = HttpResponse(cal.as_string())
-    response['Content-Type'] = 'text/calendar'
+    response['Content-Type'] = 'text/calendar; charset=UTF-8'
+    response['Cache-Control'] = 'no-cache, no-store, max-age=0, must-revalidate'
+    response['Pragma'] = 'no-cache'
+    response['Expires'] = 'Fri, 01 Jan 1990 00:00:00 GMT'
     return response
 
