69990f6b4e37b8247fb7e865a1e81bffa4f0888b
cko <ch.ko123@googlemail.com>
2010-10-16 10:56:31 +0200

    Morgen, Übermorgen bei Datumsangaben

--- techism2/web/templatetags/web_tags.py
+++ techism2/web/templatetags/web_tags.py
@@ -1,15 +1,30 @@
+ #!/usr/local/bin/python
+ # -*- coding: utf-8 -*-
 from django.template import Library 
 from django.template.defaultfilters import stringfilter
-from datetime import date
+from datetime import date, timedelta
 
 register = Library()
 
 @register.filter
-def display_date(value):
-    if not value:
+def display_date(event_date):
+    if not event_date:
         return '';
-    elif (value.date()== date.today()):
-        return "Heute, " + value.strftime("%Y-%m-%d %H:%M")
+    elif (__is_today(event_date)):
+        return "Heute, " + event_date.strftime("%H:%M")
+    elif (__is_tomorrow(event_date)):
+        return "Morgen, " + event_date.strftime("%H:%M")
+    elif (__is_the_day_after_tomorrow(event_date)):
+        return "Übermorgen, " + event_date.strftime("%H:%M")
     else:
-        return value.strftime("%Y-%m-%d %H:%M")
+        return event_date.strftime("%Y-%m-%d %H:%M")
+    
 
+def __is_today(event_date):
+    return event_date.date() == date.today()
+    
+def __is_tomorrow(event_date):
+    return event_date.date() == date.today() + timedelta(days=1)
+    
+def __is_the_day_after_tomorrow(event_date):
+    return event_date.date() == date.today() + timedelta(days=2)
