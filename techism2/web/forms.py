5f71d901da60775f525d9f6fa7d239d8353ef5cf
cko <ch.ko123@googlemail.com>
2010-10-17 17:31:15 +0200

    validate end-date

--- techism2/web/forms.py
+++ techism2/web/forms.py
@@ -22,6 +22,12 @@ class EventForm(forms.Form):
 
     def clean(self):
         cleaned_data = self.cleaned_data
+        self.clean_Location()
+        self.clean_end_date()
+        return cleaned_data 
+    
+    def clean_Location (self):
+        cleaned_data = self.cleaned_data
         location_name = cleaned_data.get("location_name")
         location_street = cleaned_data.get("location_street")
         location_city = cleaned_data.get("location_city")
@@ -36,5 +42,12 @@ class EventForm(forms.Form):
             if not location_city:
                 self._errors["location_city"] = self.error_class([u'Alle Location Felder m\u00FCssen gef\u00FCllt sein.'])
                 del cleaned_data["location_city"]
-        
-        return cleaned_data 
+  
+                
+    def clean_end_date (self):
+        cleaned_data = self.cleaned_data
+        date_time_begin = cleaned_data.get("date_time_begin");
+        date_time_end = cleaned_data.get("date_time_end")
+        if date_time_end < date_time_begin:
+                self._errors["date_time_end"] = self.error_class([u'Das Ende-Datum muss nach dem Beginn-Datum liegen.'])
+                del cleaned_data["date_time_end"]
