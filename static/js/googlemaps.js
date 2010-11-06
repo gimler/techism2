3124aee7c06e0640f910d07c5ed5889400fca8be
seelmann <mail@stefan-seelmann.de>
2010-11-06 22:20:15 +0100

    Static/dynamic map, depending on width

--- static/js/googlemaps.js
+++ static/js/googlemaps.js
@@ -30,3 +30,58 @@ function initializeMunichCityCenter() {
   };
   map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);  
 }
+
+function renderEventDetailMap() {
+	var where = $(this).children("section.where");
+	var map_id = "map_" + where[0].id;
+	
+	// only load the map once
+	if( $("#"+map_id).length > 0) {
+		return;
+	}
+	
+	// extract query string and 'q' parameter from link
+	var a = where.find('a')
+	var query = $.parseQuery(a[0].search, {'f':function(v){return v;}}).q
+	
+	if(where.width() < 640) {
+		// static map with link for small screens, max. size of static map is 640x640
+		var width = where.width();
+		var height = Math.round(width / 3);
+		where.append('<a id="'+map_id+'" href="http://maps.google.de/maps?q='+query+'&z=17"><img src="http://maps.google.com/maps/api/staticmap?center='+query+'&size='+width+'x'+height+'&zoom=15&sensor=false&markers=color:red|'+query+'" /></a>');
+	}
+	else {
+		// dynamic map for larger screens
+		where.append('<div id="'+map_id+'" style="height: 200px; width: 100%" />');
+		
+		var myOptions = {
+				zoom: 15,
+				center: new google.maps.LatLng(48.13788,11.575953),
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		map = new google.maps.Map(document.getElementById(map_id), myOptions);
+		
+		var geocoder = new google.maps.Geocoder();
+		geocoder.geocode( { 'address': decodeURIComponent(query)}, function(results, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				map.setCenter(results[0].geometry.location);
+				var marker = new google.maps.Marker({
+					map: map, 
+					position: results[0].geometry.location,
+					draggable: false
+				});
+			}
+		});
+	}
+}
+
+
+jQuery.parseQuery = function(qs,options) {
+	var q = (typeof qs === 'string'?qs:window.location.search), o = {'f':function(v){return unescape(v).replace(/\+/g,' ');}}, options = (typeof qs === 'object' && typeof options === 'undefined')?qs:options, o = jQuery.extend({}, o, options), params = {};
+	jQuery.each(q.match(/^\??(.*)$/)[1].split('&'),function(i,p){
+		p = p.split('=');
+		p[1] = o.f(p[1]);
+		params[p[0]] = params[p[0]]?((params[p[0]] instanceof Array)?(params[p[0]].push(p[1]),params[p[0]]):[params[p[0]],p[1]]):p[1];
+	});
+	return params;
+}
--- static/js/templates/index.js
+++ static/js/templates/index.js
@@ -1,26 +1,29 @@
 $(document).ready(function(){
+
 $(".detail").hide(); 
+
 /*$("#admin").hide();*/
-/* clicks for touch interface */                 
+
+/* clicks for touch interface */
 $('.event').live('click', function() {
-    //console.log("click!");
-    $(this).children(".detail").slideToggle(300);
+	//console.log("click!");
+	$(this).children(".detail").slideToggle(300, renderEventDetailMap)
 }); 
 
 
 /*infinite scrolling*/
 $('#content').infinitescroll({
-  navSelector  : "a#nextpage:last",                  // selector for the paged navigation (it will be hidden)
-  nextSelector : "a#nextpage:last",                 // selector for the NEXT link (to page 2)
-  itemSelector : ".event",                         // selector for all items you'll retrieve
+  navSelector  : "a#nextpage:last",				  // selector for the paged navigation (it will be hidden)
+  nextSelector : "a#nextpage:last",				 // selector for the NEXT link (to page 2)
+  itemSelector : ".event",						 // selector for all items you'll retrieve
 },function(arrayOfNewElems){
-                    console.log("call");
-                      $(".detail").hide();
-                    var showevents = $(".event").size();
-                    var eventsqty = num_pages *2;
-                    if (showevents>eventsqty) $("#loadmore").hide(); 
-                     console.log(showevents); 
-                });
+	console.log("call");
+	$(".detail").hide();
+	var showevents = $(".event").size();
+	var eventsqty = num_pages *2;
+	if (showevents>eventsqty) $("#loadmore").hide(); 
+	console.log(showevents); 
+});
 // kill scroll binding
 $(window).unbind('.infscr');
 // hook up the manual click guy.
@@ -33,4 +36,6 @@ $(document).ajaxError(function(e,xhr,opt){
   if (xhr.status == 404) $('#nextpage a').remove();
 });
 
-});   
+});
+
+
